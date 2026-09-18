f70b81cc8b62076a0f5d5ee905c5fe6e7a3abc7b
attasaeed <atta.saeed77@gmail.com>
2026-09-18 16:20:21 +0500

    Design builder page

--- client/src/pages/BuilderPage.jsx
+++ client/src/pages/BuilderPage.jsx
@@ -1,9 +1,74 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import Loading from "../components/Loading";
+import BuilderHeader from "../components/BuilderHeader";
+import { useAppContext } from "../context/AppContext";
 
 const BuilderPage = () => {
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const [leftTab, setLeftTab] = useState("chat");
+  const [publishing, setPublishing] = useState(false);
+  const [publishUrl, setPublishUrl] = useState(null);
+
+  const {
+    activeProject,
+    loadingActiveProject,
+    activeFile,
+    showCode,
+    setActiveFile,
+    setShowCode,
+    loadProject,
+    logout,
+  } = useAppContext();
+
+  useEffect(() => {
+    if (!id) return;
+    loadProject(id);
+  }, [id]);
+
+  useEffect(() => {
+    if (!id || !activeProject) return;
+    if (
+      activeProject.status === "pending" ||
+      activeProject.status === "generating"
+    ) {
+      const interval = setInterval(() => {
+        loadProject(id, true);
+      }, 1500);
+      return () => clearInterval(interval);
+    }
+  }, [id, loadProject, activeProject]);
+
+  const handleOpenPreview = () => {
+    if (!id) return;
+    window.open(`/preview/${id}`, "_blank");
+  };
+
+  if (loadingActiveProject || !activeProject) {
+    return <Loading />;
+  }
+
+  const handlePublish = () => {};
+  const handleDownload = () => {};
+
   return (
-    <div>
-      <h1>BuilderPage</h1>
+    <div className="h-screen flex flex-col bg-white overflow-hidden text-zinc-900 relative">
+      {/* Top bar Header */}
+      <BuilderHeader
+        projectName={activeProject.name}
+        version={activeProject.version}
+        showCode={showCode}
+        publishing={publishing}
+        onToggleShowCode={() => setShowCode(!showCode)}
+        onOpenPreview={handleOpenPreview}
+        onPublish={handlePublish}
+        onDownload={handleDownload}
+        onBack={() => navigate("/")}
+        onLogout={logout}
+      />
+
+      {/* Main Layout */}
     </div>
   );
 };
